a24541fce3455d225fc4c593ad15354a4828b1c2
ach02raf <57291591+ach02raf@users.noreply.github.com>
2023-12-26 14:01:41 +0000

    config i18n 26-12-2023

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio-angular';
+  constructor(private translateService:TranslateService )
+  {
+    const userLang=navigator.language || 'en';
+    const languageCode=userLang.split('-')[0];
+    this.translateService.setDefaultLang(languageCode);
+    this.translateService.use(languageCode);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,30 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { TranslateLoader,TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient,HttpClientModule } from '@angular/common/http';
+
+export function HttpLoaderFactory(http:HttpClient){
+ return new TranslateHttpLoader(http,"./assets/i18n/",".json");
+}
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader:{
+             provide:TranslateLoader,
+             useFactory:HttpLoaderFactory,
+             deps:[HttpClient],
+      }
+    })
   ],
   providers: [
     provideClientHydration()
