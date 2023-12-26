2f507066239d4a581c6fd1bb5c2ce5bd5f4f2cb7
Safa <30405481+safachaabane@users.noreply.github.com>
2023-12-26 13:17:17 +0000

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { LastProjectsComponent } from './components/last-projects/last-projects.component';
+import { ServicesComponent } from './components/services/services.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { SliderComponent } from './components/slider/slider.component';
 
 import { TranslateLoader,TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
@@ -15,7 +25,17 @@ export function HttpLoaderFactory(http:HttpClient){
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProjectsComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutMeComponent,
+    SkillsComponent,
+    LastProjectsComponent,
+    ServicesComponent,
+    NavBarComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
