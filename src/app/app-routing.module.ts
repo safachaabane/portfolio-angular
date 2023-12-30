736229d796d5cc4e2465600d87779e44f73714d9
Safa <30405481+safachaabane@users.noreply.github.com>
2023-12-30 20:49:44 +0000

    create langage detect guard 30-12-2023

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { languageGuard } from './guards/language.guard';
+import { HomeComponent } from './pages/home/home.component';
+import { languageDetectGuard } from './guards/language-detect.guard';
 
 const routes: Routes = [
+  { path: '', children: [], canActivate:[languageDetectGuard], component: AppComponent },
   {
     path: ':lang',
     pathMatch: 'full',
     canActivate: [languageGuard],
-    component: AppComponent,
+    component: HomeComponent,
   },
 ];
 
--- src/app/guards/language-detect.guard.ts
+++ src/app/guards/language-detect.guard.ts
@@ -1,6 +1,38 @@
+import { Injectable, inject } from '@angular/core';
 import { ActivatedRouteSnapshot,RouterStateSnapshot, CanActivateFn } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { Router } from '@angular/router';
 
+@Injectable({
+  providedIn: 'root', 
+})
+
+class translateServiceDetect {
+  constructor(private translateService: TranslateService,  private router: Router){}
+  detect(languageCode:string):boolean {
+    this.translateService.setDefaultLang(languageCode);
+    this.translateService.use(languageCode);
+    this.router.navigate([languageCode]);
+    //  this.translateService.get("name").subscribe({
+    //   next: (res)=>{ console.log(res);
+    //   },
+    // })    
+    return true;
+  }
+  notDetect(): boolean{
+    this.translateService.setDefaultLang('en');
+    this.translateService.use('en');
+    this.router.navigate(["en"]);    
+    return true;
+  }
+}
 export const languageDetectGuard: CanActivateFn =(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
-  const lang = route.params['lang'];
-  return /^(en|fr|de)$/.test(lang);
+  if (typeof navigator !== 'undefined') {
+    const userLang = navigator.language || 'en';
+    const languageCode = userLang.split('-')[0];
+    return inject(translateServiceDetect).detect(languageCode); 
+  } else {
+    return inject(translateServiceDetect).notDetect();
+
+  }
 };
