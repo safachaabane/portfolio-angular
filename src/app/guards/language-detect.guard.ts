65bc15dacbe87388956d0b7fe9e5579f5c010f75
Safa <30405481+safachaabane@users.noreply.github.com>
2023-12-31 13:11:55 +0000

    update gaurd 31-12-2023

--- src/app/guards/language-detect.guard.ts
+++ src/app/guards/language-detect.guard.ts
@@ -1,45 +1,55 @@
 import { Injectable, inject } from '@angular/core';
-import { ActivatedRouteSnapshot,RouterStateSnapshot, CanActivateFn } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  CanActivateFn,
+} from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { Router } from '@angular/router';
 import { LoadingService } from '../services/loading.service';
 
 @Injectable({
-  providedIn: 'root', 
+  providedIn: 'root',
 })
-
 class translateServiceDetect {
-  constructor(private translateService: TranslateService,  private router: Router,private loadingService :LoadingService){}
-  detect(languageCode:string):boolean {
-    this.loadingService.showLoading();
+  constructor(
+    private translateService: TranslateService,
+    private router: Router,
+    private loadingService: LoadingService
+  ) {}
+  detect(languageCode: string): boolean {
     this.translateService.setDefaultLang(languageCode);
     this.translateService.use(languageCode);
     setTimeout(() => {
       this.router.navigate([languageCode]);
       this.loadingService.hideLoading();
     }, 1000);
-   
+
     return true;
   }
-  notDetect(): boolean{
-    this.loadingService.showLoading();
+  notDetect(): boolean {
     this.translateService.setDefaultLang('en');
     this.translateService.use('en');
-      
     setTimeout(() => {
-      this.router.navigate(["en"]);
+      this.router.navigate(['en']);
       this.loadingService.hideLoading();
     }, 1000);
     return true;
   }
+  show() {
+    this.loadingService.showLoading();
+  }
 }
-export const languageDetectGuard: CanActivateFn =(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+export const languageDetectGuard: CanActivateFn = (
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+) => {
+  inject(translateServiceDetect).show();
   if (typeof navigator !== 'undefined') {
     const userLang = navigator.language || 'en';
     const languageCode = userLang.split('-')[0];
-    return inject(translateServiceDetect).detect(languageCode); 
+    return inject(translateServiceDetect).detect(languageCode);
   } else {
     return inject(translateServiceDetect).notDetect();
-
   }
 };
