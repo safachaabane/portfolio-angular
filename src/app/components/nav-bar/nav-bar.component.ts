b3b0cc90a9a7e312801b405cf24c9b74571b4ad1
Safa Chaabane <30405481+safachaabane@users.noreply.github.com>
2024-01-02 21:39:11 +0000

    configuration of translate

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,33 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component} from '@angular/core';
+import { TranslateDetectService } from '../../services/translate-detect.service';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
-export class NavBarComponent implements OnInit {
-  jsonData: any; 
+export class NavBarComponent {
 
-  constructor(private httpClient: HttpClient) {}
-
+  constructor(private translateDetectService: TranslateDetectService) {}
+  url : String = "";
   ngOnInit() {
-    const currentLanguage = localStorage.getItem('lang') || 'en';
-    const jsonFilePath = `assets/i18n/${currentLanguage}.json`;
-
-    this.httpClient.get(jsonFilePath).subscribe(
-      (data: any) => {
-        this.jsonData = data?.menu;
-       
-      },
-      error => {
-        console.error(`Error loading ${currentLanguage}.json:`, error);
-      }
-    );
+  this.url="/"+localStorage.getItem("lang");
+ 
   }
 
-  onChange(language: string) {
+  async onChange(language: string) {
+    localStorage.clear();
     localStorage.setItem('lang', language);
-    this.ngOnInit();
+    this.translateDetectService.detect(language)
   }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,29 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-  jsonData: any; // Assuming this will hold the loaded JSON data
+export class HomeComponent{
 
-  constructor(private httpClient: HttpClient) {}
+  constructor() {}
 
   ngOnInit() {
-    const currentLanguage = localStorage.getItem('lang') || 'en';
-    const jsonFilePath = `assets/i18n/${currentLanguage}.json`;
-
-    this.httpClient.get(jsonFilePath).subscribe(
-      (data: any) => {
-        // Assuming you want to store the loaded JSON data in a variable
-        this.jsonData = data?.menu;
-        console.log(this.jsonData)
-      },
-      error => {
-        console.error(`Error loading ${currentLanguage}.json:`, error);
-      }
-    );
   }
 }
