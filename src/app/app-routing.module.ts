2df8f41303a281005452b791f38bacc7bfb078c2
safach <safa1chaabane@gmail.com>
2024-02-12 06:20:39 +0100

    filling the json

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,28 +9,48 @@ import { ProjectComponent } from './pages/project/project.component';
 import { BlogComponent } from './pages/blog/blog.component';
 
 const routes: Routes = [
-  { path: '', children: [], component: AppComponent },
- 
   {
-    path: ':lang',
-    pathMatch: 'full',
-    canActivate: [languageGuard],
-    component:HomeComponent  
+    path: '',
+    children: [
+      {
+        path: '',
+        component: AppComponent,
+        pathMatch:'full'
+      },
+      {
+        path: ':lang',
+        pathMatch: 'full',
+        canActivate: [languageGuard],
+        component: HomeComponent,
+      },
+      {
+        path: ':lang/projects',
+        children: [
+          { path: '', component: ProjectsComponent },
+          { path: ':urlProject', component: ProjectComponent },
+        ],
+      },
+      {
+        path: ':lang/blogs',
+        children: [{ path: ':urlBlog', component: BlogComponent }],
+      },
+    ],
   },
-  { path: ':lang/projects',
-    children:[ {path:'', component: ProjectsComponent}, {path:':urlProject', component: ProjectComponent}]},
-    { path: ':lang/blogs',
-    children:[{path:':urlBlog', component: BlogComponent}]},
- 
+
   { path: '404', component: ErrorComponent },
   {
     path: '**',
-   redirectTo:'404'
-  }
+    redirectTo: '404',
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes,{ scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled' })],
+  imports: [
+    RouterModule.forRoot(routes, {
+      scrollPositionRestoration: 'enabled',
+      anchorScrolling: 'enabled',
+    }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
